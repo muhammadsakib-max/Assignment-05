e3a011cf0ef9fb88e9a2687b00d244d1e3f34ea2
MD. SAKIB <muhammadsakib079@gmail.com>
2026-09-11 08:40:56 +0600

    Footer Base Design is Ready (Responsive)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -9,6 +10,7 @@ function App() {
     <>
     < Navbar />
     < Hero />
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,52 @@
+import Logo from '../assets/logo-text.png'
+const Footer = () => {
+  return (
+    <><footer className="footer sm:footer-horizontal bg-base-200  border-t border-gray-300 text-base-content p-10 ">
+  <aside>
+    <img src={Logo} alt="Logo" />
+    <p>
+      Curated tools, technologies, and resources for developers building
+
+      <br />
+      modern software.
+    </p>
+    <div className="grid grid-flow-col gap-4">
+        <a className="font-bold" href="">GitHub</a>
+        <a className="font-bold"  href="">Twitter</a>
+        <a className="font-bold" href="">LinkedIn</a>
+    </div>
+  </aside>
+  <nav>
+    <h6 className="footer-title">PRODUCT</h6>
+    <a className="link link-hover">Home</a>
+    <a className="link link-hover">Technologies</a>
+    <a className="link link-hover">Projects</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Company</h6>
+    <a className="link link-hover">About</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Careers</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+  </nav>
+</footer>
+
+
+<footer className="footer sm:footer-horizontal border-t border-gray-300 bg-base-200 p-4 max-sm:place-items-center max-sm:text-center">
+  <aside className="grid-flow-col items-center">
+    <p>© {new Date().getFullYear()} - Dev Stack. All rights reserved.</p>
+  </aside>
+  <nav className="grid-flow-col gap-4 sm:place-self-center sm:justify-self-end">
+    <a className="link link-hover">Privacy</a>
+    <a className="link link-hover">Terms</a>
+  </nav>
+</footer>
+</>
+  )
+}
+
+export default Footer
